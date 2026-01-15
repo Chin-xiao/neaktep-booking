23e490af03191a0eb2972a60efe2edd9e717a917
@YoYo_Coding <132632904+PhinChansophal@users.noreply.github.com>
2026-01-15 19:55:20 +0700

    Update hello in row profile

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,8 @@ class _MainNavigationState extends State<MainNavigation> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.book_online), label: "My Booking"),
-          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble_outline), label: "Message"),djdfghsgdfsdty
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble_outline), label: "Message"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Hello User"),
         ],
       ),
     );
